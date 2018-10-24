afbbad9c86bd06fa3b5494b3358856845c34a903
David Jiang <djiang86@binghamton.edu>
2018-10-24 20:06:55 +0000

    ghost button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { WarningButtonComponent } from './buttons/warning-button/warning-button.component';
-import { DangerButtonComponent } from './buttons/danger-button/danger-button.component';
-import { SuccessButtonComponent } from './buttons/success-button/success-button.component';
-import { PrimaryButtonComponent } from './buttons/primary-button/primary-button.component';
-import { MultiOptionButtonComponent } from './buttons/multi-option-button/multi-option-button.component';
+import { WarningButtonComponent } from './buttons/regular-buttons/warning-button/warning-button.component';
+import { DangerButtonComponent } from './buttons/regular-buttons/danger-button/danger-button.component';
+import { SuccessButtonComponent } from './buttons/regular-buttons/success-button/success-button.component';
+import { PrimaryButtonComponent } from './buttons/regular-buttons/primary-button/primary-button.component';
+import { MultiOptionButtonComponent } from './buttons/regular-buttons/multi-option-button/multi-option-button.component';
+import { GhostButtonRectComponent } from './buttons/ghost-buttons/ghost-button-rect/ghost-button-rect.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { MultiOptionButtonComponent } from './buttons/multi-option-button/multi-
     DangerButtonComponent,
     SuccessButtonComponent,
     PrimaryButtonComponent,
-    MultiOptionButtonComponent
+    MultiOptionButtonComponent,
+    GhostButtonRectComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/buttons/ghost-buttons/ghost-button-rect/ghost-button-rect.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-ghost-button-rect',
+  templateUrl: './ghost-button-rect.component.html',
+  styleUrls: ['./ghost-button-rect.component.css']
+})
+export class GhostButtonRectComponent implements OnInit {
+  @Input() buttonText: string;
+  text: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.text = this.buttonText ? this.buttonText : 'Ghost';
+  }
+
+}
